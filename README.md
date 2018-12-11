c38f3886bf6754239701e305811386acc5832760
l-bevc <lbevc@pmfst.hr>
2018-12-11 15:00:17 +0100

    Second level completed

--- README.md
+++
@@ -1,2 +0,0 @@
-# FirstApp
-Learning Angular
--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     ServerComponent,
-    AppComponent
+    AppComponent,
+    ServersComponent
   ],
   imports: [
     FormsModule,
